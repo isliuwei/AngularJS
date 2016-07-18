8da053e0fba4913b09f3d6a690ae2aafbe6999af
isliuwei <lwdgzyx@hotmail.com>
2016-07-18 21:46:32 +0800

    新增笔记
    ng-repeat ng-show ng-hide

---
+++ AngularJS权威指南笔记/js/repeat.js
@@ -0,0 +1,28 @@
+
+
+var myApp = angular.module('myApp',[]);
+
+myApp.controller('bookCtrl',['$scope',function($scope){
+    $scope.books = [
+      {
+        'ID': "0010",
+        'name': "《javascript高级程序设计》",
+        'author': "佚名"
+      },
+      {
+        'ID': "0010",
+        'name': "《javascript高级程序设计》",
+        'author': "佚名"
+      },
+      {
+        'ID': "0010",
+        'name': "《javascript高级程序设计》",
+        'author': "佚名"
+      },
+      {
+        'ID': "0010",
+        'name': "《javascript高级程序设计》",
+        'author': "佚名"
+      }
+    ];
+}]);
